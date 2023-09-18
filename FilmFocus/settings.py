efb07955deb005d2c93a6223b9618aa2647b575b
Mark Peterson <mark.peterson@ku.edu>
2023-09-18 00:53:55 -0500

    minor fixes

--- FilmFocus/settings.py
+++ FilmFocus/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+ekqg-+(cfeagt66@&@x_c6x1j8hh@6v*!8d-%qywo67u$&^n1'
+load_dotenv()
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- webapp/services.py
+++ webapp/services.py
@@ -1,3 +1,16 @@
 # All requests from external services/website/API here
 
 from webapp.models import *
+from dotenv import load_dotenv
+
+
+## Example API request
+# def scoresSearch(daysFrom="3"):
+#     load_dotenv()
+#     ODDS_API = os.environ["ODDS_API"]
+
+#     params = {"daysFrom": daysFrom, "apiKey": ODDS_API}
+#     baseUrl = "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/scores"
+#     response = requests.get(baseUrl, params=params)
+#     json = response.json()
+#     return json
