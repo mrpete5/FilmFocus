dc5ea15e7644f9e24b4bfa9bc025761c07a99422
Mark Peterson <mark.peterson@ku.edu>
2023-09-19 18:25:55 -0500

    removed unused html pages, renamed a couple

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,11 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("index2/", views.index2, name="index2"),
-    path("details1/", views.details1, name="details1"),
-    path("details2/", views.details2, name="details2"),
-    path("catalog1/", views.catalog1, name="catalog1"),
-    path("catalog2/", views.catalog2, name="catalog2"),
+    path("details/", views.details, name="details"),
+    path("catalog/", views.catalog, name="catalog"),
     path("about/", views.about, name="about"),
     path("404/", views.four04, name="404"),
     path("signin/", views.signin, name="signin"),
--- webapp/views.py
+++ webapp/views.py
@@ -7,20 +7,11 @@ from .services import *
 def index(request):
     return render(request, "index.html")
 
-def index2(request):
-    return render(request, "index2.html")
+def details(request):
+    return render(request, "details.html")
 
-def details1(request):
-    return render(request, "details1.html")
-
-def details2(request):
-    return render(request, "details2.html")
-
-def catalog1(request):
-    return render(request, "catalog1.html")
-
-def catalog2(request):
-    return render(request, "catalog2.html")
+def catalog(request):
+    return render(request, "catalog.html")
 
 def about(request):
     return render(request, "about.html")
