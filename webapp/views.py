1a085e9dbe0b90d8ed6405819efe2bcc0cdf9ae6
Mark Peterson <mark.peterson@ku.edu>
2023-09-19 20:26:23 -0500

    fix Populate movie slots on homepage, ReqID 4 #5

--- webapp/views.py
+++ webapp/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render, HttpResponse
 from .models import Movie
 from .services import *
+import random
 
-
-# Create your views here.
 def index(request):
-    return render(request, "index.html")
+    # Fetch the 20 newest releases
+    newest_releases = Movie.objects.all().order_by('-release_year')[:20]
+    
+    # Fetch 8 random movies from the 20 newest releases for "New Movies"
+    new_movies = random.sample(list(newest_releases), 8)
+    
+    # Fetch 6 random movies for "Popular Movies", excluding the ones already selected
+    popular_movies = Movie.objects.all().exclude(id__in=[movie.id for movie in new_movies]).order_by('?')[:6]
+    
+    # Fetch 12 random movies for "More Movies", excluding the ones already selected in new_movies and popular_movies
+    more_movies = Movie.objects.all().exclude(id__in=[movie.id for movie in new_movies] + [movie.id for movie in popular_movies]).order_by('?')[:12]
+    
+    context = {
+        'new_movies': new_movies,
+        'popular_movies': popular_movies,
+        'more_movies': more_movies,
+    }
+    return render(request, 'index.html', context)
+
 
 def details(request):
     return render(request, "details.html")
@@ -35,7 +52,8 @@ def movies(request):
     
     database_initialized = True    # always make True before committing changes
     if database_initialized == False:
-        initialize_movie_database(page_count=2)     # 20 movies per page
+        initialize_movie_database(page_count=10)     # 20 movies per page
     
-    items = Movie.objects.all()
+    items = Movie.objects.all().order_by('?')[:20]  # Fetch only the first 20 movies
     return render(request, "movies.html", {"movies": items})
+
