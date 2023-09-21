5f91abb4fbd4110a2721c9123b9599883d2f7eac
Mark Peterson <mark.peterson@ku.edu>
2023-09-21 18:45:01 -0500

    fix new_release bug & clean process_movie_search

--- webapp/services.py
+++ webapp/services.py
@@ -47,64 +47,72 @@ def search_and_fetch_movie_by_id(tmdb_id):
 
 
 def process_movie_search(tmdb_id, title, now_playing=False):
-    if tmdb_id:
-        # Check if the movie is in the ban list
-        if str(tmdb_id) in BAN_LIST:
-            print(f"Movie '{title}' (ID: {tmdb_id}) is in the ban list and was not added.")
-            return
-
-        # Make an API call to fetch more details about the movie from TMDB
-        movie_details = fetch_movie_details_from_tmdb(tmdb_id)
-        trailer_key = fetch_movie_trailer_key(tmdb_id)  # Fetch the trailer key from TMDB
-        
-        # Check if the movie is not adult content
-        if not movie_details.get('adult'):
-            # Extract genres
-            genres = movie_details.get('genres', [])
-            
-            # Extract release year from release_date
-            release_date = movie_details.get('release_date')
-            release_year = int(release_date.split('-')[0]) if release_date else None
-            
-            # Check if the movie exists in the database
-            if not Movie.objects.filter(tmdb_id=tmdb_id).exists():
-                # Create the movie object with TMDB data
-                movie = Movie.objects.create(
-                    tmdb_id=movie_details.get('id'),
-                    title=movie_details.get('title'),
-                    overview=movie_details.get('overview'),
-                    poster_path=movie_details.get('poster_path'),
-                    release_year=release_year,
-                    runtime=movie_details.get('runtime'),
-                    tagline=movie_details.get('tagline'),
-                    trailer_key=trailer_key,
-                    now_playing=now_playing  # Set the now_playing field
-                )
-                
-                # Fetch additional data from OMDB
-                omdb_data = fetch_movie_data_from_omdb(title)
-                movie.imdb_rating = omdb_data.get('imdb_rating')
-                movie.rotten_tomatoes_rating = omdb_data.get('rotten_tomatoes_rating')
-                movie.metacritic_rating = omdb_data.get('metacritic_rating')
-                movie.director = omdb_data.get('director')
-                movie.domestic_box_office = omdb_data.get('domestic_box_office')
-                movie.mpa_rating = omdb_data.get('mpa_rating')
-                movie.save()
-                
-                # Associate the movie with its genres from TMDB
-                for genre_data in genres:
-                    genre, created = Genre.objects.get_or_create(name=genre_data['name'])
-                    movie.genres.add(genre)
-                
-                print(f"Movie '{title}' (ID: {tmdb_id}) fetched and saved to the database.")
-            else:
-                # If the movie already exists, update its now_playing status
-                Movie.objects.filter(tmdb_id=tmdb_id).update(now_playing=now_playing)
-                print(f"Movie '{title}' (ID: {tmdb_id}) already exists in the database.")
-        else:
-            print(f"Movie '{title}' (ID: {tmdb_id}) is for adults and was not added.")
-    else:
+    # Check if the movie is in the TMDB master list
+    if not tmdb_id:
         print(f"Movie '{title}' not found in the master list.")
+        return
+    
+    # Check if the movie is in the ban list
+    if str(tmdb_id) in BAN_LIST:
+        print(f"Movie '{title}' (ID: {tmdb_id}) is in the ban list and was not added.")
+        return
+
+    # Make an API call to fetch more details about the movie from TMDB
+    movie_details = fetch_movie_details_from_tmdb(tmdb_id)
+    
+    # Check if the movie is adult content
+    if movie_details.get('adult'):
+        print(f"Movie '{title}' (ID: {tmdb_id}) is for adults and was not added.")
+        return
+            
+    # Check if the movie exists in the database
+    if Movie.objects.filter(tmdb_id=tmdb_id).exists():
+        # If the movie already exists, update its now_playing status
+        Movie.objects.filter(tmdb_id=tmdb_id).update(now_playing=now_playing)
+        print(f"Movie '{title}' (ID: {tmdb_id}) already exists in the database.")
+        return
+
+    # Fetch the trailer key from TMDB
+    trailer_key = fetch_movie_trailer_key(tmdb_id)
+    # Extract release year from release_date
+    release_date = movie_details.get('release_date')
+    release_year = int(release_date.split('-')[0]) if release_date else None
+    
+    # Verify now_playing is either 2023 or 2024
+    if now_playing and release_year not in [2023, 2024]:
+        now_playing = False
+    
+    # Create the movie object with TMDB data
+    movie = Movie.objects.create(
+        tmdb_id=movie_details.get('id'),
+        title=movie_details.get('title'),
+        overview=movie_details.get('overview'),
+        poster_path=movie_details.get('poster_path'),
+        release_year=release_year,
+        runtime=movie_details.get('runtime'),
+        tagline=movie_details.get('tagline'),
+        trailer_key=trailer_key,
+        now_playing=now_playing,
+    )
+    
+    # Fetch additional data from OMDB
+    omdb_data = fetch_movie_data_from_omdb(title)
+    movie.imdb_rating = omdb_data.get('imdb_rating')
+    movie.rotten_tomatoes_rating = omdb_data.get('rotten_tomatoes_rating')
+    movie.metacritic_rating = omdb_data.get('metacritic_rating')
+    movie.director = omdb_data.get('director')
+    movie.domestic_box_office = omdb_data.get('domestic_box_office')
+    movie.mpa_rating = omdb_data.get('mpa_rating')
+    movie.save()
+
+    # Extract genres
+    genres = movie_details.get('genres', [])        
+    # Associate the movie with its genres from TMDB
+    for genre_data in genres:
+        genre, created = Genre.objects.get_or_create(name=genre_data['name'])
+        movie.genres.add(genre)
+    
+    print(f"Movie '{title}' (ID: {tmdb_id}) fetched and saved to the database.")
 
 
 def fetch_movie_details_from_tmdb(tmdb_id):
@@ -202,9 +210,6 @@ def fetch_now_playing_movies():
             title = movie.get('title')
             # Process each movie using the process_movie_search function
             process_movie_search(tmdb_id, title, now_playing=True)
-            
-        if page_num != 5:  # No need to sleep after fetching the last page
-            time.sleep(1)
 
 
 def get_movies_for_index():
