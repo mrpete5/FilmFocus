84f4ecc1df03014cc1b8cf633151044768b5020e
Mark Peterson <mark.peterson@ku.edu>
2023-09-24 22:54:22 -0500

    admin register new models

--- webapp/admin.py
+++ webapp/admin.py
@@ -3,3 +3,7 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Movie)
+admin.site.register(Genre)
+admin.site.register(UserProfile)
+admin.site.register(Watchlist)
+admin.site.register(WatchlistEntry)
