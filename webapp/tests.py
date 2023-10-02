945ba4239f229f13d87f3f02f1a895ba80f563fc
Mark Peterson <mark.peterson@ku.edu>
2023-10-02 17:05:15 -0500

    initialize tests.py

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,3 +1,65 @@
+"""
+Name of code artifact: tests.py
+Brief description: Contains all tests and testing logic for the FilmFocus web application, 
+                    including multiple edge cases, logging, documentation. Made to be very 
+                    adaptive and easy to understand.
+Programmer’s name: Mark
+Date the code was created: 10/02/2023
+Dates the code was revised: 10/06/2023
+Brief description of each revision & author: Initialized code and basic structure (Mark).
+Preconditions: Django environment must be set up correctly, and necessary environment variables (API keys) must be available.
+Acceptable and unacceptable input values or types: Functions expect specific types as documented in their respective comments.
+Postconditions: Functions return values or modify the database as per their documentation.
+Return values or types: Varies based on the function.
+Error and exception condition values or types that can occur: Errors can occur if API limits are reached or if there are issues with the database.
+Side effects: Some functions modify the database.
+Invariants: None.
+Any known faults: None.
+"""
+
+"""
+    # TODO: Write your tests here.
+            - Logging:
+                - Add logging so failures display to the console.
+                - Add logging so results of the full test are written to a new file.
+                - Output file format should be:
+                    - Format: "output_MM_DD_YYYY_HH_mm_ss.txt" with the date and time. (Allow for 24 hour clock).
+                    - Example: output_10_02_2023_17_30_07.txt (Default, or something similar.)
+            - Documentation:
+            - Edge cases:
+            - Write some tests that you would like to test.
+            - Write some edge cases that you would like to test.
+            
+"""
+
+
 from django.test import TestCase
 
 # Create your tests here.
+class Test():
+    
+    def __init__(self) -> None:
+        ''' Constructor for the Test class. '''
+        # TODO: Implement the constructor for the Test class.
+        
+        
+        pass        # 'pass' is a reserved word in Python to skip the function.
+    
+    def run(self) -> None:
+        ''' Runs the test. '''
+        # TODO: Implement the run function for the Test class.
+       
+        pass
+
+    def test_display(self) -> None:
+        ''' Display the test page. '''
+        # TODO: Implement the test_display function within the Test class.
+       
+        pass
+
+def main():
+    my_test = Test()    # Create a new instance of the Test class.
+    my_test.run()       # Run the test.
+    
+if __name__ == "__main__":
+    main()
