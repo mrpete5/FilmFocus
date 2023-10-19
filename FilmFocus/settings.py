0ec43b3d29e277b961cbb14a1fd05b3d3ed63941
bill-laughlin <123513007+bill-laughlin@users.noreply.github.com>
2023-10-19 17:19:34 -0500

    updated settings.py

--- FilmFocus/settings.py
+++ FilmFocus/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddlewhere',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'FilmFocus.urls'
