f827c2d9a36b5fde755b59313073fe865cd13c36
johnzheng0 <jzheng1777@gmail.com>
2023-12-03 16:59:10 -0600

    frontpage new releases carousel

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -104,18 +104,23 @@ $(document).ready(function () {
 		responsive : {
 			0 : {
 				items: 2,
+				slideBy: 2,
 			},
 			576 : {
 				items: 2,
+				slideBy: 2,
 			},
 			768 : {
 				items: 3,
+				slideBy: 3,
 			},
 			992 : {
 				items: 4,
+				slideBy: 4,
 			},
 			1200 : {
 				items: 4,
+				slideBy: 4,
 			},
 		}
 	});
